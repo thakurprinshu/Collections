ee543c3ff29112f0b7594fa4d647e01b35aa9300
Priyanshu Thakur <thakurprinshu1349@gmail.com>
2025-08-28 15:19:34 +0530

    Added me code for removing duplicates

---
+++ src/arraylist/RemDuplicates.java
@@ -0,0 +1,20 @@
+package arraylist;
+
+import java.util.*;
+
+public class RemDuplicates {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 2, 5, 3, 6, 1));
+        HashSet<Integer> seen = new HashSet<>();
+        HashSet<Integer> duplicates = new HashSet<>();
+
+        for (int num : list) {
+            if (!seen.add(num)) {
+                duplicates.add(num);
+            }
+        }
+
+        System.out.println("Original List: " + list);
+        System.out.println("Without Duplicates: " + seen);
+    }
+}
