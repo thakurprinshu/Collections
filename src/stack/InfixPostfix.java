ea08928b415938869305e87fa61350a012f89c7a
Priyanshu Thakur <thakurprinshu1349@gmail.com>
2025-08-28 10:39:13 +0530

    Added the code to convert infix to post fix

---
+++ src/stack/InfixPostfix.java
@@ -0,0 +1,55 @@
+package stack;
+
+import java.util.*;
+
+public class InfixPostfix {
+
+    static int precedence(char ch) {
+        switch (ch) {
+            case '+':
+            case '-': return 1;
+            case '*':
+            case '/': return 2;
+            case '^': return 3;
+        }
+        return -1;
+    }
+
+    static String infixToPostfix(String expression) {
+        StringBuilder result = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < expression.length(); i++) {
+            char ch = expression.charAt(i);
+
+            if (Character.isLetterOrDigit(ch)) {
+                result.append(ch);
+            }
+            else if (ch == '(') {
+                stack.push(ch);
+            }
+            else if (ch == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    result.append(stack.pop());
+                }
+                stack.pop();
+            }
+            else {
+                while (!stack.isEmpty() && precedence(ch) <= precedence(stack.peek())) {
+                    result.append(stack.pop());
+                }
+                stack.push(ch);
+            }
+        }
+        while (!stack.isEmpty()) {
+            result.append(stack.pop());
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        String infix = "a+b*(c^d-e)^(f+g*h)-i";
+        System.out.println("Infix Expression:   " + infix);
+        System.out.println("Postfix Expression: " + infixToPostfix(infix));
+    }
+}
