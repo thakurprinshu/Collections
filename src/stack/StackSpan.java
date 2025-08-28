564111703f6fbf14ff78ab02321f845d3fa8a1a6
Priyanshu Thakur <thakurprinshu1349@gmail.com>
2025-08-28 10:42:32 +0530

    added the code for Stock span calc

---
+++ src/stack/StackSpan.java
@@ -0,0 +1,33 @@
+package stack;
+
+import java.util.*;
+
+public class StackSpan {
+    public static int[] calculateSpan(int[] prices) {
+        int n = prices.length;
+        int[] span = new int[n];
+        Stack<Integer> stack = new Stack<>(); // stores indices of days
+        span[0] = 1;
+        stack.push(0);
+
+        for (int i = 1; i < n; i++) {
+            while (!stack.isEmpty() && prices[stack.peek()] <= prices[i]) {
+                stack.pop();
+            }
+            span[i] = stack.isEmpty() ? (i + 1) : (i - stack.peek());
+
+            stack.push(i);
+        }
+
+        return span;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {100, 80, 60, 70, 60, 75, 85};
+        int[] span = calculateSpan(prices);
+
+        System.out.println("Stock Prices: " + Arrays.toString(prices));
+        System.out.println("Span Values : " + Arrays.toString(span));
+    }
+}
+
